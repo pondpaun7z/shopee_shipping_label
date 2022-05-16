248b82b9739cb2fcdf3cd687295942aff34a5be2
Theeraphat Jantakat <pppangpondpp7@gmail.com>
2022-05-16 09:27:52 +0700

    fix: crop shipping label

--- app/services/custom_shipping_label_service.rb
+++ app/services/custom_shipping_label_service.rb
@@ -13,24 +13,24 @@ class CustomShippingLabelService
     file_key = file.original_filename.gsub(".pdf", "")
     pdf = MiniMagick::Image.open(file.path)
 
-    pdf.pages.each do |page|
+    pdf.pages.each.with_index do |page, index|
       MiniMagick::Tool::Convert.new do |convert|
         convert.background "white"
         convert.flatten
         convert.density 150
         convert.quality 100
         convert << page.path
-        convert << Rails.root.join("tmp", "shipping_labels", "label-#{file_key}.jpg").to_s
+        convert << Rails.root.join("tmp", "shipping_labels", "label-#{file_key}-#{index}.jpg").to_s
       end
 
-      shipping_label_path = Rails.root.join("tmp", "shipping_labels", "label-#{file_key}.jpg").to_s
+      shipping_label_path = Rails.root.join("tmp", "shipping_labels", "label-#{file_key}-#{index}.jpg").to_s
       shipping_label = MiniMagick::Image.open(shipping_label_path)
 
-      qrcode_file_path = Rails.root.join("tmp", "shipping_labels", "qrcode-#{file_key}.jpg")
+      qrcode_file_path = Rails.root.join("tmp", "shipping_labels", "qrcode-#{file_key}-#{index}.jpg")
       shipping_label.crop "485x195+700+0-200-0"
       shipping_label.write qrcode_file_path
 
-      combile_file = Rails.root.join("tmp", "shipping_labels", "combile_file-#{file_key}.jpg")
+      combile_file = Rails.root.join("tmp", "shipping_labels", "combile_file-#{file_key}-#{index}.jpg")
 
       composite_image = MiniMagick::Image.open(shipping_label_path)
       image = MiniMagick::Image.open(qrcode_file_path)
@@ -50,10 +50,10 @@ class CustomShippingLabelService
     output_filename = "custom-#{file.original_filename}"
     output_path = Rails.root.join("tmp", "custom_shipping_labels", output_filename).to_s
     Prawn::Document.generate(output_path, page_size: "A4") do
-      pdf.pages.count.times do |i|
-        combile_file = Rails.root.join("tmp", "shipping_labels", "combile_file-#{file_key}.jpg")
+      pdf.pages.count.times do |index|
+        combile_file = Rails.root.join("tmp", "shipping_labels", "combile_file-#{file_key}-#{index}.jpg")
         image combile_file, width: 600, at: [-35, 800]
-        start_new_page if i < pdf.pages.count - 1
+        start_new_page if index < pdf.pages.count - 1
       end
     end
   end
